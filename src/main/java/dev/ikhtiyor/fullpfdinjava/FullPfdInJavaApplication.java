ddddb51a2c00794e3927728b7448c59518e1fc5a
ikhtiyordev <ikhtiyordev@gmail.com>
2021-11-01 00:06:52 +0500

    create project

---
+++ src/main/java/dev/ikhtiyor/fullpfdinjava/FullPfdInJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.fullpfdinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullPfdInJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullPfdInJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ikhtiyor/fullpfdinjava/FullPfdInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.fullpfdinjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullPfdInJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
